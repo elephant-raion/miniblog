a600bb632de6dd5473abe1e448951f9492edf6a3
Yasuhiro Sugawara <toreharose@gmail.com>
2022-03-30 23:44:22 +0900

    controllerとviewをkaminari gem仕様にした

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,7 +2,7 @@ class MicropostsController < ApplicationController
   # GET /microposts
   def index
     @micropost = Micropost.new
-    @pagy, @microposts = pagy(Micropost.all.order(id: :desc))
+    @microposts = Micropost.order(id: :desc).page params[:page]
   end
 
   # POST /microposts
@@ -12,7 +12,7 @@ class MicropostsController < ApplicationController
     if @micropost.save
       redirect_to microposts_path, flash: { success: '投稿に成功しました' }
     else
-      @pagy, @microposts = pagy(Micropost.all.order(id: :desc))
+      @microposts = Micropost.order(id: :desc).page params[:page]
       flash[:error] = '投稿に失敗しました'
       render :index
     end
