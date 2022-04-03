41a667f49ed92d9a74a9c8c4d7a4c720d6ea1c72
Yasuhiro Sugawara <toreharose@gmail.com>
2022-04-04 07:13:42 +0900

    ログインにユーザー名を用いる

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,10 @@ class User < ApplicationRecord
   validates :profile, length: { maximum: 200 }
   validates :url, url: { allow_blank: true }
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         authentication_keys: [:name]
+
+  def self.find_for_database_authentication(conditions)
+    where(conditions).first
+  end
 end
