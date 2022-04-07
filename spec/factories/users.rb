481a15f8188a7e4848e7e0ecaf3585dcd678cfe0
Yasuhiro Sugawara <toreharose@gmail.com>
2022-04-08 00:15:51 +0900

    アルファベットの変換メソッドをリファクタリングした

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,17 @@
-def lowercase_alphabets
-  ('a'..'z').to_a
+class Integer
+  def to_alphabet
+    to_s(26).tr('0-9a-p', 'a-z')
+  end
 end
 
 FactoryBot.define do
   factory :user do
     sequence :email do |n|
-      "tester#{lowercase_alphabets[n - 1]}@example.com"
+      "tester#{(n - 1).to_alphabet}@example.com"
     end
     password { 'password' }
     sequence :name do |n|
-      "tester#{lowercase_alphabets[n - 1]}"
+      "tester#{(n - 1).to_alphabet}"
     end
     profile { '' }
     blog_url { '' }
