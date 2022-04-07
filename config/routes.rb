f6e77def2965683c7732ac20af733e15cc3f8530
Yasuhiro Sugawara <toreharose@gmail.com>
2022-04-07 19:47:53 +0900

    不要なコントローラーの指定を削除した

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root 'microposts#index'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    sessions: 'users/sessions',
   }
 
   resources :microposts, only: %i[index create]
